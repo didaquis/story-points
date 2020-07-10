f72bc290b9d45144b88ce677e8723c7e9cbae80e
Dídac García <didaquis@gmail.com>
2020-07-10 17:57:10 +0200

    Added estimation result box

--- src/components/EstimationResultBox.vue
+++ src/components/EstimationResultBox.vue
@@ -1,3 +1,10 @@
 <template>
-	<p>Estimation Result Box</p>
+	<div class="card">
+		<div class="card-body">
+			<h3 class="card-title text-dark text-center my-0">3</h3>
+		</div>
+		<div class="card-footer text-muted">
+			<span role="img" aria-label="Aviso" class="mr-2">⚠️</span> Considera dividir esta incidencia en otras más pequeñas!
+		</div>
+	</div>
 </template>
