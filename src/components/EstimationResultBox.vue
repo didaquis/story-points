b76d82736dec98d9db4b4b77ae3372d3a5266ea8
Dídac García <didaquis@gmail.com>
2020-07-16 00:32:11 +0200

    Added basic logic to result box

--- src/components/EstimationResultBox.vue
+++ src/components/EstimationResultBox.vue
@@ -1,10 +1,27 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h3 class="card-title text-dark text-center my-0">3</h3>
+			<h3 class="card-title text-dark text-center my-0">{{ estimationValue }}</h3>
 		</div>
-		<div class="card-footer text-muted">
+		<div class="card-footer text-muted" v-show="isBigEstimation">
 			<span role="img" aria-label="Aviso" class="mr-2">⚠️</span> Considera dividir esta incidencia en otras más pequeñas!
 		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'EstimationResultBox',
+	props: {
+		estimationValue: {
+			type: [String, Number],
+			default: ''
+		}
+	},
+	data() {
+		return {
+			isBigEstimation: this.estimationValue >= 8
+		}
+	}
+}
+</script>
