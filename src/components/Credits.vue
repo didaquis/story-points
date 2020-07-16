7aaa64df35da7bb4e88df54176b0ecbcb6d13042
Dídac García <didaquis@gmail.com>
2020-07-16 20:55:33 +0200

    Improved About page

---
+++ src/components/Credits.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="alert alert-primary mt-5">
+		<p>Este proyecto ha sido realizado por <a target="_blank" class="alert-link" rel="noopener" href="https://didaquis.github.io">Dídac García</a>.</p>
+		<p class="mb-0">Puedes encontrar el código fuente en <a target="_blank" class="alert-link" rel="noopener" href="https://github.com/didaquis/story-points">GitHub</a>.</p>
+	</div>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,32 @@
 <template>
-	<div>
-		<Title text="About this app"/>
-		<p class="lead">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
-		<h3>How it works?</h3>
-		<p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
-		<h3>How it works?</h3>
-		<p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+	<div class="pb-2">
+		<Title text="Información"/>
+		<p class="lead">Story Points es una aplicación que te permite realizar estimaciones</p>
+		<h3>¿Por qué se ha creado esta aplicación?</h3>
+		<p>A veces los equipos Scrum tienen problemas para encontrar aquellos criterios para estimar historias de usuario. Esta aplicación trata de simplificar el proceso para conseguir estimaciones más precisas.</p>
+		<h3>¿Cómo se usa?</h3>
+		<p>Cuando necesites realizar una estimación, sitúate en la pantalla inicial y selecciona el nivel de incertidumbre y el nivel de volumen que consideres que tiene esa tarea. La aplicación te mostrará el resultado de tu estimación de manera automática.
+		<br>
+		Si en algún momento, consideras que no estás capacitado para determinar el valor de incertidumbre y/o volumen, pulsa el botón con el símbolo de interrogación para indicar que no puedes estimar esa tarea.</p>
+		<Credits />
 	</div>
 </template>
 
 <script>
 import Title from '@/components/Title.vue'
+import Credits from '@/components/Credits.vue'
 
 export default {
 	name: 'About',
 	components: {
-		Title
+		Title,
+		Credits,
 	}
 }
 </script>
+
+<style scoped>
+h3 {
+	margin-top: 1.2em;
+}
+</style>
