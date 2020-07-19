608608cbf2cc8a9b4468da79b268566bbe787cb6
Dídac García <didaquis@gmail.com>
2020-07-19 14:55:47 +0200

    Fix navbar glitch

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,17 +1,17 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4 border-bottom border-white" data-cy="navbar">
 		<div class="container">
-			<router-link class="navbar-brand" to="/" data-cy="brand-link">Story points</router-link>
+			<router-link class="navbar-brand" to="/" data-cy="brand-link" data-toggle="collapse" data-target=".navbar-collapse.show">Story points</router-link>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" data-cy="navbar-burguer" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNav" data-cy="navbar-burguer-links">
 				<ul class="navbar-nav">
 					<li class="nav-item">
-						<router-link class="nav-link" data-toggle="collapse" data-target="#navbarNav" to="/">Inicio</router-link>
+						<router-link class="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" to="/">Inicio</router-link>
 					</li>
 					<li class="nav-item">
-						<router-link class="nav-link" data-toggle="collapse" data-target="#navbarNav" to="/info">Información</router-link>
+						<router-link class="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show" to="/info">Información</router-link>
 					</li>
 				</ul>
 			</div>
