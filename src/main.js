90fd96a6b3da77e23a17690a2db7480448f83259
Dídac García <didaquis@gmail.com>
2020-10-11 14:43:14 +0200

    Added basic manifest

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import App from './App.vue'
 import router from './router'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
