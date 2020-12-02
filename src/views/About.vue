9b1033b104f27ac75a8f538a75ea5940bf468214
Dídac García <didaquis@gmail.com>
2020-12-02 23:18:15 +0100

    Texts has been improved

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
 	<section>
 		<Title text="Información"/>
 		<p class="lead">Story Points es una aplicación que te permite realizar estimaciones</p>
-		<h3>¿Por qué se ha creado esta aplicación?</h3>
+		<h3>¿Por qué he creado esta aplicación?</h3>
 		<p>A veces los equipos Scrum tienen problemas para encontrar aquellos criterios para estimar historias de usuario. Esta aplicación trata de simplificar el proceso para conseguir estimaciones más precisas.</p>
 		<h3>¿Cómo se usa?</h3>
 		<p>Cuando necesites realizar una estimación, sitúate en la pantalla inicial y selecciona el nivel de incertidumbre y el nivel de volumen que consideres que tiene esa tarea. La aplicación te mostrará el resultado de tu estimación de manera automática.
